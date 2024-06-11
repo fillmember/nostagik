06c4914df00f24e671c1e571afead9d9be96ee11
ten <fillmember@gmail.com>
2024-06-11 18:59:50 +0200

    fix(react): correct built file names

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,29 +1,20 @@
 const { withNx } = require('@nx/rollup/with-nx');
-const url = require('@rollup/plugin-url');
-const svg = require('@svgr/rollup');
 
 module.exports = withNx(
   {
     main: './src/index.tsx',
     outputPath: '../../dist/packages/react',
     tsConfig: './tsconfig.lib.json',
-    compiler: 'babel',
+    compiler: 'tsc',
     external: ['react', 'react-dom', 'react/jsx-runtime', 'clsx'],
-    format: ['esm', 'cjs'],
+    format: ['cjs', 'esm'],
     assets: [{ input: '.', output: '.', glob: 'README.md' }],
+    outputFileName: 'index',
     generateExportsField: true,
+    skipTypeField: false,
+    skipTypeCheck: false,
   },
   {
     // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
-    plugins: [
-      svg({
-        svgo: false,
-        titleProp: true,
-        ref: true,
-      }),
-      url({
-        limit: 10000, // 10kB
-      }),
-    ],
   }
 );
