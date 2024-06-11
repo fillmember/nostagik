e3d8698a59af6ee0182f7188eb34f69b4bbabb28
ten <fillmember@gmail.com>
2024-06-11 14:07:33 +0200

    test: add createRenderConfig test

--- packages/nostagik/jest.config.ts
+++ packages/nostagik/jest.config.ts
@@ -1,11 +1,14 @@
 /* eslint-disable */
-export default {
+
+const config = {
   displayName: 'nostagik',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
+  moduleFileExtensions: ['ts', 'js'],
   coverageDirectory: '../../coverage/packages/nostagik',
 };
+
+export default config;
---
+++ packages/nostagik/src/lib/config.test.ts
@@ -0,0 +1,19 @@
+import { createRenderConfig, defaultRenderConfig } from './config';
+
+describe('config', () => {
+  describe('createRenderConfig', () => {
+    test('should merge recursively', () => {
+      const input = { notionBlockClasses: { prefix: 'yee-' } };
+      const output = createRenderConfig(input);
+      expect(output.notionAnnotationsClasses).toEqual(
+        defaultRenderConfig.notionAnnotationsClasses
+      );
+      expect(output.fullWidthImageCondition).toEqual(
+        defaultRenderConfig.fullWidthImageCondition
+      );
+      expect(output.notionBlockClasses.map).toEqual(
+        defaultRenderConfig.notionBlockClasses.map
+      );
+    });
+  });
+});
