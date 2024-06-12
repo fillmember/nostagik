fe95946d2bfdcb2b9e2c39a1ae0671fef32a152e
ten <fillmember@gmail.com>
2024-06-12 17:08:14 +0200

    fix: correct tailwind.config.js

--- apps/nostagik-site/tailwind.config.js
+++ apps/nostagik-site/tailwind.config.js
@@ -1,5 +1,4 @@
 import { join } from 'path';
-import { createGlobPatternsForDependencies } from '@nx/react/tailwind';
 import configForNostagik from '@nostagik/core/src/tailwind-preset';
 
 /** @type {import('tailwindcss').Config} */
@@ -10,17 +9,17 @@ module.exports = {
       __dirname,
       '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
     ),
-    ...createGlobPatternsForDependencies(__dirname),
-    ...configForNostagik.content,
+    join(
+      __dirname,
+      '../../packages/nostagik/src/**/*!(*.stories|*.spec).{tsx,ts,jsx,js,html}'
+    ),
+    join(
+      __dirname,
+      '../../packages/react/src/**/*!(*.stories|*.spec).{tsx,ts,jsx,js,html}'
+    ),
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-console.log('=======');
-console.log('=======');
-console.log(module.exports.content);
-console.log('=======');
-console.log('=======');
