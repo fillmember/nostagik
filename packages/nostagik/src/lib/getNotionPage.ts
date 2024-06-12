05578d4913a728b6c4fc790045c24252a0879b67
ten <fillmember@gmail.com>
2024-06-12 15:38:45 +0200

    fix(core): correct dependencies and update tests

--- packages/nostagik/src/lib/getNotionPage.ts
+++ packages/nostagik/src/lib/getNotionPage.ts
@@ -11,7 +11,7 @@ export const defaultGetNotionPageOption: GetNotionPageOption = {
     image: './public/cached-images',
     imagePublicPath: './cached-images',
   },
-  notionClientAuthToken: null,
+  notionClientAuthToken: undefined,
 };
 
 export function resolveGetNotionPageOption(
