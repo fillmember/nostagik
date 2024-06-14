b3c602a327e7175da3f6c7bb241fadc05c9cd78b
ten <fillmember@gmail.com>
2024-06-14 12:05:23 +0200

    fix(core): only expose richTextToString from utils file
    the others should be internal

--- packages/nostagik/src/lib/nostagik.ts
+++ packages/nostagik/src/lib/nostagik.ts
@@ -2,4 +2,4 @@ export * from './getNotionPage';
 export * from './getAllPages';
 export * from './types';
 export * from './config';
-export * from './utils';
+export { richTextToString } from './utils';
